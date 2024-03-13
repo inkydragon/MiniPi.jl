77a90ec56dad5b8f0bf4ab2b6d7b57cb4545ba50
Chengyu HAN <git@wo-class.cn>
2024-03-12 20:54:02 -0500

    bigFloat: impl MiniBf

--- src/bigFloat.jl
+++ src/bigFloat.jl
@@ -1,9 +1,67 @@
 
-struct MiniBf <: AbstractFloat end
+"Precision increase step."
+const EXTRA_PRECISION = UInt64(2)
 
-function MiniBf() end
-function MiniBf(bf::MiniBf) end
-function MiniBf(x::UInt32, sign=true) end
+"""
+The Big Floating-point object.
+It represents an arbitrary precision floating-point number.
+
+
+Its numerical value is equal to:
+```
+    word = 10^9
+    word^exp * (T[0] + T[1]*word + T[2]*word^2 + ... + T[L - 1]*word^(L - 1))
+```
+
+T is an array of 32-bit integers. Each integer stores 9 decimal digits
+and must always have a value in the range `[0, 999999999]`.
+
+`T[L - 1]` must never be zero. 
+
+The number is positive when (sign = true) and negative when (sign = false).
+Zero is represented as (sign = true) and (len = 0).
+"""
+mutable struct MiniBf <: AbstractFloat
+    "true = positive or zero; false = negative"
+    sign::Bool
+    "Exponent"
+    exp::Int64
+    "Length"
+    len::UInt64
+    
+    tab::Vector{UInt32}
+end
+
+function MiniBf()
+    MiniBf(true, zero(Int64), zero(UInt64), UInt32[])
+end
+MiniBf(bf::MiniBf) = bf
+
+"""
+    MiniBf(x::UInt32, sign=true)
+
+Construct a BigFloat with a value of x and the specified sign.
+"""
+function MiniBf(x::UInt32, sign=true)
+    bf = MiniBf(true, zero(Int64), one(UInt64), UInt32[])
+
+    if iszero(x)
+        bf.len = 0
+    else
+        bf.sign = sign
+        bf.tab = UInt32[x]
+    end
+
+    @assert length(bf.tab) == bf.len
+    bf
+end
+
+Base.:(==)(x::MiniBf, y::MiniBf) =
+    x.sign == y.sign &&
+    x.exp == y.exp &&
+    x.len == y.len &&
+    x.tab == y.tab
+
 
 function to_string(bf::MiniBf, digits=0) end
 function to_string_sci(bf::MiniBf, digits=0) end
--- test/bigFloat.jl
+++ test/bigFloat.jl
@@ -1,4 +1,26 @@
+import MiniPi.MiniBf
 
 @testset "bigFloat.jl" begin
 
+end
+
+@testset "MiniBf" begin
+    # Types
+    @test MiniBf <: AbstractFloat
+    @test MiniBf() isa AbstractFloat
+
+    @test MiniBf() == MiniBf()
+    @test MiniBf(MiniBf()) == MiniBf()
+    @test MiniBf() == MiniBf(true, 0, 0x0000000000000000, UInt32[])
+
+    for x in [zero(UInt32), rand(UInt32, 9)...],
+        sign in [true, false]
+        @testset "MiniBf($x, $sign)" begin
+            if iszero(x)
+                @test MiniBf(x, sign) == MiniBf(true, zero(Int64), zero(UInt64), UInt32[])
+            else
+                @test MiniBf(x, sign) == MiniBf(sign, zero(Int64), one(UInt64), UInt32[x])
+            end
+        end
+    end
 end
