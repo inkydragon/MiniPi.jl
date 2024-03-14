51939f361197a6d5a8dfbbb1d4ee1a8a20b2c18f
Chengyu HAN <git@wo-class.cn>
2024-03-13 20:13:07 -0500

    bigFloat: group MiniBf testset

--- test/bigFloat.jl
+++ test/bigFloat.jl
@@ -7,36 +7,18 @@ import MiniPi:
 end
 
 @testset "MiniBf" begin
-    # Types
+    #= Types =#
     @test MiniBf <: AbstractFloat
     @test MiniBf() isa AbstractFloat
 
-    # MiniBf()
+    #= MiniBf() =#
     @test MiniBf() == MiniBf()
-    # MiniBf(::MiniBf)
-    @test MiniBf(MiniBf()) == MiniBf()
     @test MiniBf() == MiniBf(true, 0, 0x0000000000000000, UInt32[])
-    
-    # MiniBf(::Integer)
-    # Bool
-    @test MiniBf(true) == MiniBf(UInt32(1))
-    @test MiniBf(false) == MiniBf(UInt32(0))
-    # Unsigned
-    @test MiniBf(0xffff) == MiniBf(UInt32(0xffff))
-    @test MiniBf(0xffffffff) == MiniBf(UInt32(0xffffffff))
-    @test MiniBf(0x00000000ffffffff) == MiniBf(UInt32(0x00000000ffffffff))
-    # Signed
-    @test MiniBf(typemax(Int16)) == MiniBf(UInt32(typemax(Int16)))
-    @test MiniBf(typemin(Int16)+1) == MiniBf(UInt32(32768-1), false)
-    @test MiniBf(typemax(Int32)) == MiniBf(UInt32(typemax(Int32)))
-    @test MiniBf(typemin(Int32)+1) == MiniBf(UInt32(2147483648-1), false)
-    @test MiniBf(Int64(typemax(UInt32))) == MiniBf(UInt32(4294967295))
-    @test MiniBf(-Int64(typemax(UInt32))) == MiniBf(UInt32(4294967295), false)
-    @test MiniBf(Int128(typemax(UInt32))) == MiniBf(UInt32(4294967295))
-    @test MiniBf(-Int128(typemax(UInt32))) == MiniBf(UInt32(4294967295), false)
-    @test MiniBf(BigInt(typemax(UInt32))) == MiniBf(UInt32(4294967295))
-    @test MiniBf(-BigInt(typemax(UInt32))) == MiniBf(UInt32(4294967295), false)
 
+    #= MiniBf(::MiniBf) =#
+    @test MiniBf(MiniBf()) == MiniBf()
+
+    #= MiniBf(::UInt32, ::Bool=true) =#
     for x in [zero(UInt32), rand(UInt32, 9)...],
         sign in [true, false]
         if iszero(x)
@@ -55,6 +37,26 @@ end
             @test MiniBf(x, sign) == MiniBf(sign, zero(Int64), one(UInt64), UInt32[x])
         end
     end
+
+    #= MiniBf(::Integer) =#
+    # Bool
+    @test MiniBf(true) == MiniBf(UInt32(1))
+    @test MiniBf(false) == MiniBf(UInt32(0))
+    # Unsigned
+    @test MiniBf(0xffff) == MiniBf(UInt32(0xffff))
+    @test MiniBf(0xffffffff) == MiniBf(UInt32(0xffffffff))
+    @test MiniBf(0x00000000ffffffff) == MiniBf(UInt32(0x00000000ffffffff))
+    # Signed
+    @test MiniBf(typemax(Int16)) == MiniBf(UInt32(typemax(Int16)))
+    @test MiniBf(typemin(Int16)+1) == MiniBf(UInt32(32768-1), false)
+    @test MiniBf(typemax(Int32)) == MiniBf(UInt32(typemax(Int32)))
+    @test MiniBf(typemin(Int32)+1) == MiniBf(UInt32(2147483648-1), false)
+    @test MiniBf(Int64(typemax(UInt32))) == MiniBf(UInt32(4294967295))
+    @test MiniBf(-Int64(typemax(UInt32))) == MiniBf(UInt32(4294967295), false)
+    @test MiniBf(Int128(typemax(UInt32))) == MiniBf(UInt32(4294967295))
+    @test MiniBf(-Int128(typemax(UInt32))) == MiniBf(UInt32(4294967295), false)
+    @test MiniBf(BigInt(typemax(UInt32))) == MiniBf(UInt32(4294967295))
+    @test MiniBf(-BigInt(typemax(UInt32))) == MiniBf(UInt32(4294967295), false)
 end
 
 @testset "Base.precision" begin
