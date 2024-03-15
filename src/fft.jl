b945c246e348ee0285237ce0b7cbc65a6146e8e7
Chengyu HAN <git@wo-class.cn>
2024-03-15 12:43:07 -0500

    fft: add and test `bit_reverse_indices`

--- src/fft.jl
+++ src/fft.jl
@@ -1,3 +1,25 @@
+"""
+A basic FFT multiply implementation. No optimizations are done.
+"""
+
+
+function bit_reverse_indices(N::Integer)
+    base = 2
+    nbits = ceil(Int, log2(N))
+    rev_indices = zeros(Int, N)
+
+    for i in 0:N-1
+        # 对索引 i 进行位反转
+        rev = reverse(digits(i, base=base, pad=nbits))
+        # 计算反转后的索引 from:  ?digits
+        rev_index = sum(rev[k]*base^(k-1) for k=1:length(rev))
+        rev_indices[i+1] = rev_index
+    end
+
+    # Julia 中索引从 1 开始
+    rev_indices .+ 1
+end
+
 
 """
     ensure_fft_tables(cl::UInt64)
--- test/fft.jl
+++ test/fft.jl
@@ -1,4 +1,15 @@
+import MiniPi:
+    bit_reverse_indices
 
 @testset "fft" begin
 
 end
+
+@testset "bit_reverse_indices" begin
+    @test bit_reverse_indices(2) == [1, 2]
+    @test bit_reverse_indices(4) == [1, 3, 2, 4]
+    @test bit_reverse_indices(8) == [1, 5, 3, 7, 2, 6, 4, 8]
+    @test bit_reverse_indices(16) ==
+        [1, 9, 5, 13, 3, 11, 7, 15, 2, 10, 6, 14, 4, 12, 8, 16]
+end
+
