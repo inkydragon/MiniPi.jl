d078a3fe2c9f72b2b8ad03c4181921c347d1fe46
Chengyu HAN <git@wo-class.cn>
2024-05-01 16:59:26 -0500

    test: add more simple mul(bf, u32) tests

--- test/bf_math.jl
+++ test/bf_math.jl
@@ -67,8 +67,13 @@ const test_cpp_mul_u32_ref = [
 ]
 
 @testset "mul(bf, u32)" begin
+    # 0 * UInt32
     @test mul(MiniBf(0), 0x0) == MiniBf()
+    @test mul(MiniBf(0), 0x1) == MiniBf()
+    @test mul(MiniBf(0), rand(UInt32)) == MiniBf()
+    # MiniBf * 0x0
     @test mul(MiniBf(1), 0x0) == MiniBf()
+    @test mul(MiniBf(42), 0x0) == MiniBf()
 
     function test_commutative(x, y)
         @assert x >= 0
